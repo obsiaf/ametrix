f9ad4d48972ae3ae17236eb619e1b1c05c018fea
prb404 <64775485+prb404@users.noreply.github.com>
2025-10-24 22:22:53 +0200

    Add OBSIACustoms function to load external files

---
+++ OBSIACustoms🟣.js
@@ -0,0 +1,30 @@
+function OBSIACustoms(filename, filetype){
+ if (filetype=="js"){ //if filename is a external JavaScript file
+  var fileJs=document.createElement('script')
+  fileJs.setAttribute("type","text/javascript")
+  fileJs.setAttribute("src", filename)
+ }
+ else if (filetype=="css"){ //if filename is an external CSS file
+  var fileref=document.createElement("link")
+  fileref.setAttribute("rel", "stylesheet")
+  fileref.setAttribute("type", "text/css")
+  fileref.setAttribute("href", filename)
+ }
+ if (typeof fileref!="undefined"){
+  document.getElementsByTagName("head")[0].appendChild(fileref)
+ }
+ else if (typeof fileJs!="undefined"){
+  document.getElementsByTagName("body")[0].appendChild(fileJs)
+ }
+}
+
+// 🎨 external CSS
+//OBSIACustoms("https://url.com/.../customStyleSheet.css", "css") ////dynamically load and add this .css file
+// OBSIACustoms("https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css", "css")
+OBSIACustoms("https://inspe-master-meef-neo.github.io/ametice/customs/BetterCall🟣.css", "css")
+
+// 🧩 external JS
+// OBSIACustoms("https://url.com/.../customJavaScript.js", "js") //dynamically load and add this .js file
+// OBSIACustoms("https://cdn.logwork.com/widget/countdown.js", "js")
+OBSIACustoms("https://inspe-master-meef-neo.github.io/ametice/customs/extras/globlol.js", "js")
+
